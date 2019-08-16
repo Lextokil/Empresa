6e7ae699fea13c9d18a7900566e16efd53456b87
Noturno <lucablobe@gmail.com>
2019-08-15 21:48:34 -0300

    Model

--- src/main/java/br/com/Api_Empresa/Model/Funcionario.java
+++ src/main/java/br/com/Api_Empresa/Model/Funcionario.java
@@ -2,9 +2,9 @@ package br.com.Api_Empresa.Model;
 
 public class Funcionario {
 	
-	private long id;
-	private String nm_funcionario;
-	private double sl_funcionario;
+	private long idFuncionario;
+	private String nmFuncionario;
+	private double tpFuncionario;
 	
 	
 	
@@ -14,34 +14,34 @@ public class Funcionario {
 
 	
 	public long getId_funcionario() {
-		return id;
+		return idFuncionario;
 	}
 
 	public void setId_funcionario(long id_funcionario) {
-		this.id = id_funcionario;
+		this.idFuncionario = id_funcionario;
 	}
 
 	public String getNm_funcionario() {
-		return nm_funcionario;
+		return nmFuncionario;
 	}
 
 	public void setNm_funcionario(String nm_funcionario) {
-		this.nm_funcionario = nm_funcionario;
+		this.nmFuncionario = nm_funcionario;
 	}
 
 	public double getSl_funcionario() {
-		return sl_funcionario;
+		return tpFuncionario;
 	}
 
 	public void setSl_funcionario(double sl_funcionario) {
-		this.sl_funcionario = sl_funcionario;
+		this.tpFuncionario = sl_funcionario;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + (int) (idFuncionario ^ (idFuncionario >>> 32));
 		return result;
 	}
 
@@ -54,7 +54,7 @@ public class Funcionario {
 		if (getClass() != obj.getClass())
 			return false;
 		Funcionario other = (Funcionario) obj;
-		if (id != other.id)
+		if (idFuncionario != other.idFuncionario)
 			return false;
 		return true;
 	}
--- src/main/java/br/com/Api_Empresa/Model/Mecanico.java
+++ src/main/java/br/com/Api_Empresa/Model/Mecanico.java
@@ -2,7 +2,9 @@ package br.com.Api_Empresa.Model;
 
 public class Mecanico extends Funcionario {
 
+	private long id;
 	private String stFuncionario;
+	
 
 	public String getStFuncionario() {
 		return stFuncionario;
@@ -12,5 +14,26 @@ public class Mecanico extends Funcionario {
 		this.stFuncionario = stFuncionario;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mecanico other = (Mecanico) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 		
 }
