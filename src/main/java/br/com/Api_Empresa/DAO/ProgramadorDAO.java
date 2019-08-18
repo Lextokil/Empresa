2b507d9e9451d2ac58d7de8b3fd496f8dfce4c5e
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2019-08-18 19:23:38 -0300

    Adicionando ProgramadorDAO, finished!

--- src/main/java/br/com/Api_Empresa/DAO/ProgramadorDAO.java
+++ src/main/java/br/com/Api_Empresa/DAO/ProgramadorDAO.java
@@ -9,6 +9,8 @@ import java.util.List;
 import br.com.Api_Empresa.Data.ConexaoJDBC;
 import br.com.Api_Empresa.Data.ConexaoMysqlJDBC;
 import br.com.Api_Empresa.Model.Programador;
+import br.com.Api_Empresa.Model.util.TipoFuncionario;
+
 
 public class ProgramadorDAO {
 
@@ -19,32 +21,36 @@ public class ProgramadorDAO {
 	}
 
 	public Long inserir(Programador programador) throws SQLException, ClassNotFoundException {
-		Long idProgramador = null;
-		String sqlQuery = "INSERT INTO programador (nm_programador, sl_programador) VALUES (?, ?) ; ";
+		Long id_programador = null;
+		String sqlQuery = "INSERT INTO funcionario (nm_funcionario, sl_funcionario, lg_programador, tp_funcionario) VALUES (?, ?, ?, " + "\"programador\""+ ") ;";
 
 		try {
 			PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
-			stmt.setLong(1, programador.getId_programador());
-			stmt.setString(2, programador.getLg_programador());
-			stmt.execute();
+			stmt.setString(1, programador.getNm_funcionario());
+			stmt.setDouble(2, programador.getSl_funcionario());
+			stmt.setString(3, programador.getLg_programador());
 
+			stmt.execute();
+			
 			this.conexao.commit();
 		} catch (SQLException e) {
 			this.conexao.rollback();
 			throw e;
 		}
 
-		return idProgramador;
+		return id_programador;
 	}
 
 	public int alterar(Programador programador) throws SQLException, ClassNotFoundException {
-		String sqlQuery = "UPDATE programador SET nm_programador= ?, sl_programador= ? WHERE id_programador = ?";
+		String sqlQuery = "UPDATE funcionario SET nm_funcionario = ?, sl_funcionario = ?, lg_programador = ? WHERE id_funcionario = ? ";
 		int linhasAfetadas = 0;
 
 		try {
 			PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
-			stmt.setLong(1, programador.getId_programador());
-			stmt.setString(2, programador.getLg_programador());
+			stmt.setString(1, programador.getNm_funcionario());
+			stmt.setDouble(2, programador.getSl_funcionario());
+			stmt.setString(3, programador.getLg_programador());
+			stmt.setLong(4, programador.getId_funcionario());
 
 			linhasAfetadas = stmt.executeUpdate();
 			this.conexao.commit();
@@ -56,13 +62,13 @@ public class ProgramadorDAO {
 		return linhasAfetadas;
 	}
 
-	public int excluir(long idProgramador) throws SQLException, ClassNotFoundException {
+	public int excluir(long id_funcionario) throws SQLException, ClassNotFoundException {
 		int linhasAlfetadas = 0;
-		String sqlQuery = "DELETE FROM programador WHERE id_programador= ?";
+		String sqlQuery = "DELETE FROM funcionario WHERE id_funcionario = ?";
 
 		try {
 			PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
-			stmt.setLong(1, idProgramador);
+			stmt.setLong(1, id_funcionario);
 			linhasAlfetadas = stmt.executeUpdate();
 			this.conexao.commit();
 		} catch (SQLException e) {
@@ -73,12 +79,12 @@ public class ProgramadorDAO {
 		return linhasAlfetadas;
 	}
 
-	public Programador selecionar(long idProgramador) throws SQLException, ClassNotFoundException {
-		String sqlQuery = "SELECT * FROM programador WHERE id_programador = ?";
+	public Programador selecionar(long id_funcionario) throws SQLException, ClassNotFoundException {
+		String sqlQuery = "SELECT * FROM funcionario WHERE id_funcionario = ?";
 
 		try {
 			PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
-			stmt.setLong(1, idProgramador);
+			stmt.setLong(1, id_funcionario);
 			ResultSet rs = stmt.executeQuery();
 
 			if (rs.next()) {
@@ -92,31 +98,33 @@ public class ProgramadorDAO {
 	}
 
 	public List<Programador> listar() throws SQLException, ClassNotFoundException {
-		String sqlQuery = "SELECT * FROM programador ORDER BY id_programador";
+		String sqlQuery = "SELECT * FROM funcionario WHERE tp_funcionario = " + "\"programador\"" + " ORDER BY id_funcionario";
 
 		try {
 			PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
 			ResultSet rs = stmt.executeQuery();
 
-			List<Programador> programadores = new ArrayList<>();
+			List<Programador> programadors = new ArrayList<>();
 
 			while (rs.next()) {
-				programadores.add(parser(rs));
+				programadors.add(parser(rs));
 			}
 
-			return programadores;
+			return programadors;
 		} catch (SQLException e) {
 			throw e;
 		}
 	}
 
 	private Programador parser(ResultSet resultSet) throws SQLException {
-
 		Programador c = new Programador();
-		c.setId_programador(resultSet.getLong("idProgramador"));
-		c.setLg_programador(resultSet.getString("nmProgramador"));
+		
+		c.setId_funcionario(resultSet.getLong("id_funcionario"));
+		c.setNm_funcionario(resultSet.getString("nm_funcionario"));
+		c.setSl_funcionario(resultSet.getDouble("sl_funcionario"));
+		c.setLg_programador(resultSet.getString("lg_programador"));
+		c.setTp_funcionario(TipoFuncionario.valueOf(resultSet.getString("tp_funcionario")));
+
 		return c;
-
 	}
-
 }
--- src/main/java/br/com/Api_Empresa/Model/Programador.java
+++ src/main/java/br/com/Api_Empresa/Model/Programador.java
@@ -2,32 +2,24 @@ package br.com.Api_Empresa.Model;
 
 public class Programador extends Funcionario {
 
-	private long idProgramador;
-	private String lgProgramador;
+	private String lg_programador;
+
 	
-	
-	public long getId_programador() {
-		return idProgramador;
-	}
-	public void setId_programador(long idProgramador) {
-		this.idProgramador = idProgramador;
-	}
 	public String getLg_programador() {
-		return lgProgramador;
+		return lg_programador;
 	}
-	public void setLg_programador(String lgProgramador) {
-		this.lgProgramador = lgProgramador;
+	public void setLg_programador(String lg_programador) {
+		this.lg_programador = lg_programador;
 	}
-	
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + (int) (idProgramador ^ (idProgramador >>> 32));
-		result = prime * result + ((lgProgramador == null) ? 0 : lgProgramador.hashCode());
+		result = prime * result + ((lg_programador == null) ? 0 : lg_programador.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -37,26 +29,17 @@ public class Programador extends Funcionario {
 		if (getClass() != obj.getClass())
 			return false;
 		Programador other = (Programador) obj;
-		if (idProgramador != other.idProgramador)
-			return false;
-		if (lgProgramador == null) {
-			if (other.lgProgramador != null)
+		if (lg_programador == null) {
+			if (other.lg_programador != null)
 				return false;
-		} else if (!lgProgramador.equals(other.lgProgramador))
+		} else if (!lg_programador.equals(other.lg_programador))
 			return false;
 		return true;
 	}
 	
-	
-
-
-
-
-
-
-
-
-
-
+	@Override
+	public String toString() {
+		return "Programador [lg_programador=" + lg_programador + "]";
+	}
 
 }
